d59288de0cefd06108f0f7955deaaa5582a70a89
IvanABP <i-balestrieri@outlook.com>
2021-10-27 11:06:13 -0300

    Primera actividad resulta

---
+++ Activida-1/main.js
@@ -0,0 +1,8 @@
+let nota1 = parseInt(prompt("Ingrese su primera nota"));
+let nota2 = parseInt(prompt("Ingrese su segunda nota"));
+let nota3 = parseInt(prompt("Ingrese su tercera nota"));
+
+let notaFinal = (nota1 + nota2 + nota3)/3;
+
+console.log("Su notas fueron: "+ nota1 + " " + nota2 + " " + nota3);
+alert("Su notas fueron: "+ nota1 + " " + nota2 + " " + nota3 + "\n Y el promedio quedo en: "+notaFinal)
